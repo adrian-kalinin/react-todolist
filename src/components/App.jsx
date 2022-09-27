a265732a56c9ad53583a7824c31aad99842faa3c
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-27 22:09:03 +0300

    Add new column date

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,46 @@
 import { useState } from 'react';
 
 function App() {
-  const [description, setDescription] = useState('');
+  const [newTodo, setNewTodo] = useState({ description: '', date: '' });
   const [todos, setTodos] = useState([]);
 
-  const inputChanged = (event) => {
-    setDescription(event.target.value);
+  const descriptionChanged = (event) => {
+    setNewTodo({ ...newTodo, description: event.target.value });
+  };
+
+  const dateChanged = (event) => {
+    setNewTodo({ ...newTodo, date: event.target.value });
   };
 
   const addTodo = (event) => {
     event.preventDefault();
 
-    if (description) {
-      setTodos([...todos, description]);
-      setDescription('');
+    if (newTodo.description && newTodo.date) {
+      setTodos([...todos, newTodo]);
+      setNewTodo({ description: '', date: '' });
     }
   };
 
   return (
     <div className="container mt-3">
       <div className="input-group mb-3">
-        <input type="text" className="form-control" onChange={inputChanged} value={description} placeholder="Description" />
-        <button type="button" className="btn btn-outline-secondary" onClick={addTodo}>Add task</button>
+        <input type="text" className="form-control" onChange={descriptionChanged} value={newTodo.description} placeholder="Description" />
+        <input type="text" className="form-control" onChange={dateChanged} value={newTodo.date} placeholder="Date" />
+        <button type="button" className="btn btn-outline-secondary" onClick={addTodo}>Add</button>
       </div>
       <table className="table">
         <thead className="table-light">
           <tr>
             <th>Description</th>
+            <th>Date</th>
           </tr>
         </thead>
         <tbody>
           { todos.map((todo) => (
             // eslint-disable-next-line react/jsx-key
             <tr>
-              <td>{todo}</td>
+              <td>{todo.description}</td>
+              <td>{todo.date}</td>
             </tr>
           ))}
         </tbody>
