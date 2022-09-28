7390a9d7d9a9477040363360e2034387099ac256
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-28 10:53:19 +0300

    Refactor to-do form and add delete functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,6 @@ function App() {
   const [newTodo, setNewTodo] = useState({ description: '', date: '' });
   const [todos, setTodos] = useState([]);
 
-  const descriptionChanged = (event) => {
-    setNewTodo({ ...newTodo, description: event.target.value });
-  };
-
-  const dateChanged = (event) => {
-    setNewTodo({ ...newTodo, date: event.target.value });
-  };
-
   const addTodo = (event) => {
     event.preventDefault();
 
@@ -21,26 +13,54 @@ function App() {
     }
   };
 
+  const deleteTodo = (event, todoIndex) => {
+    setTodos(todos.filter((_, index) => index !== todoIndex));
+  };
+
   return (
     <div className="container mt-3">
-      <div className="input-group mb-3">
-        <input type="text" className="form-control" onChange={descriptionChanged} value={newTodo.description} placeholder="Description" />
-        <input type="text" className="form-control" onChange={dateChanged} value={newTodo.date} placeholder="Date" />
-        <button type="button" className="btn btn-outline-secondary" onClick={addTodo}>Add</button>
-      </div>
+      <form className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          onChange={(event) => setNewTodo({ ...newTodo, description: event.target.value })}
+          value={newTodo.description}
+          placeholder="Description"
+        />
+        <input
+          type="text"
+          className="form-control"
+          onChange={(event) => setNewTodo({ ...newTodo, date: event.target.value })}
+          value={newTodo.date}
+          placeholder="Date"
+        />
+        <button type="submit" className="btn btn-outline-secondary" onClick={addTodo}>
+          Add
+        </button>
+      </form>
       <table className="table">
         <thead className="table-light">
           <tr>
             <th>Description</th>
             <th>Date</th>
+            <th>{' '}</th>
           </tr>
         </thead>
         <tbody>
-          { todos.map((todo) => (
-            // eslint-disable-next-line react/jsx-key
-            <tr>
+          { todos.map((todo, index) => (
+            // eslint-disable-next-line react/jsx-key,react/no-array-index-key
+            <tr key={index}>
               <td>{todo.description}</td>
               <td>{todo.date}</td>
+              <td className="text-end">
+                <button
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={(event) => deleteTodo(event, index)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
