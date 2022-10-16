4b9085a30315c64890800fd76dff62cc821c1f4f
Adrian Kalinin <adriankalinin@protonmail.com>
2022-10-16 23:00:04 +0300

    Remove bootstrap dependency

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
