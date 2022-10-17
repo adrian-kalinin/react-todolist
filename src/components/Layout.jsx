2513b991dbb6c0dd6f42c92b84a012eb9e104292
Adrian Kalinin <adriankalinin@protonmail.com>
2022-10-17 16:24:11 +0300

    Remove bootstrap navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
 
 function Layout() {
   return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
