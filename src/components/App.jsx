6bd07b6edf0260f1150eb2ffee7ee86b48fc0006
Adrian Kalinin <adriankalinin@protonmail.com>
2022-10-23 23:19:11 +0300

    Add tabs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react';
+import { Container, Tab, Typography } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 import TaskList from './TaskList';
 
 function App() {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <TaskList />
+    <Container>
+      <TabContext value={value}>
+        <TabList onChange={handleChange} sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tab label="Home" value="1" />
+          <Tab label="Todos" value="2" />
+        </TabList>
+        <TabPanel value="1">
+          <Typography variant="h1">
+            Welcome!
+          </Typography>
+        </TabPanel>
+        <TabPanel value="2">
+          <TaskList />
+        </TabPanel>
+      </TabContext>
+    </Container>
   );
 }
 
